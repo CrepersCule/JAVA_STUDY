0f305d3a0106a85906ef52b66867c0d667e9be2d
admin <admin@10.137.138.112>
2019-01-08 17:44:21 +0900

    VariableHelpEx

---
+++ src/icehs/science/chapter03/VariableHelpEx.java
@@ -0,0 +1,17 @@
+package icehs.science.chapter03;
+
+public class VariableHelpEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a1234;
+		int first;
+		int class1;
+		int var;
+		int number = 1;
+		
+		System.out.println(number);
+		
+	}
+
+}
